86ccd0bd464dde0d5ad6e2d2a42cedf402337b3a
tabata Vasques <tabata07defaria@gmail.com>
2022-05-31 13:42:05 -0300

    solução da primeira sessão

--- secao_02_estrutura_de_decisao/ex_18_validador_de_data.py
+++ secao_02_estrutura_de_decisao/ex_18_validador_de_data.py
@@ -22,5 +22,9 @@ Faça um Programa que peça uma data no formato dd/mm/aaaa e determine se a mesm
 """
 
 
+from socket import NI_NUMERICHOST
+
+
 def validar_data(data: str):
     """Escreva aqui em baixo a sua solução"""
+  
